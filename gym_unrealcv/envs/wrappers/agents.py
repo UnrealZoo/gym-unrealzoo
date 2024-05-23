ab5fc6d882b288fe3756da14b8b7416ce49b098d
zfw1226 <zfw1226@gmail.com>
2024-05-24 00:35:33 +0800

    config_nav_mode at reset

--- gym_unrealcv/envs/wrappers/agents.py
+++ gym_unrealcv/envs/wrappers/agents.py
@@ -11,6 +11,7 @@ class NavAgents(Wrapper):
         self.nav_list = []
         self.agents = []
         self.mask_agent = mask_agent
+        self.reset()
 
     def step(self, action):
         # the action is a list of actions for each agent, the length of the action is the number of agents
@@ -43,9 +44,28 @@ class NavAgents(Wrapper):
     def reset(self, **kwargs):
         states = self.env.reset(**kwargs)
         env = self.env.unwrapped
+        self.nav_list = self.config_nav_mode(env)
+        # init agents
+        self.agents = []
+        for idx, mode in enumerate(self.nav_list):
+            if mode == -1:
+                self.agents.append(None)
+            elif mode == 0:
+                self.agents.append(RandomAgent(env.action_space[idx], 10, 50))
+            elif mode == 1:
+                self.agents.append(InternalNavAgent(env.safe_start, env.reset_area))
+            elif mode == 2:
+                # print(env.action_space[i])
+                self.agents.append(Nav2GoalAgent(env.action_space[idx], env.reset_area, max_len=200))
+        if self.mask_agent:
+            states = np.array([states[id] for id,value in enumerate(self.nav_list) if value < 0])
+            self.action_space = [self.env.action_space[i] for i, nav in enumerate(self.nav_list) if nav < 0]
+            self.observation_space = [self.env.observation_space[i] for i, nav in enumerate(self.nav_list) if nav < 0]
+        return states
 
+    def config_nav_mode(self, env):
         # set nav list
-        self.nav_list = []
+        nav_list = []
         for i, obj_name in enumerate(env.player_list):
             '''
             config the navigation mode, 
@@ -55,33 +75,15 @@ class NavAgents(Wrapper):
             2: use the goal navigation
             '''
             if i == env.tracker_id:
-                self.nav_list.append(-1)
+                nav_list.append(-1)
             elif env.agents[obj_name]['agent_type'] in ['car', 'player']:
                 if env.agents[obj_name]['internal_nav'] is True:
-                    self.nav_list.append(1)
+                    nav_list.append(1)
                 else:
-                    self.nav_list.append(2)
+                    nav_list.append(2)
             elif env.agents[obj_name]['agent_type'] == 'drone':
-                self.nav_list.append(0)
+                nav_list.append(0)
             else:
-                self.nav_list.append(2)
+                nav_list.append(2)
             # print(f'{obj_name} use mode: {self.nav_list[-1]}')
-
-        # init agents
-        self.agents = []
-        for idx, mode in enumerate(self.nav_list):
-            if mode == -1:
-                self.agents.append(None)
-            elif mode == 0:
-                self.agents.append(RandomAgent(env.action_space[i], 10, 50))
-            elif mode == 1:
-                self.agents.append(InternalNavAgent(env.safe_start, env.reset_area))
-            elif mode == 2:
-                # print(env.action_space[i])
-                self.agents.append(Nav2GoalAgent(env.action_space[i], env.reset_area, max_len=200))
-        if self.mask_agent:
-            # TODO: update the tracker_id and target_id
-            states = np.array([states[id] for id,value in enumerate(self.nav_list) if value < 0])
-            self.action_space = [self.env.action_space[i] for i, nav in enumerate(self.nav_list) if nav < 0]
-            self.observation_space = [self.env.observation_space[i] for i, nav in enumerate(self.nav_list) if nav < 0]
-        return states
+        return nav_list
