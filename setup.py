86917758c0ff9648804800d952c60a4af5faf2fd
zfw1226 <zfw1226@gmail.com>
2024-12-27 21:55:09 +0800

    add dependence (pynput), update version to 2.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='gym_unrealcv',
-      version='1.0.0',
-      install_requires=['gym<=0.21.0', 'matplotlib', 'numpy', 'unrealcv', 'wget', 'opencv-python','simple_pid','pynput'],  # And any other dependencies foo needs
+      version='2.0.0',
+      install_requires=['gym<=0.21.0', 'matplotlib', 'numpy', 'unrealcv', 'wget', 'opencv-python','simple_pid', 'pynput'],  # And any other dependencies foo needs
 )
