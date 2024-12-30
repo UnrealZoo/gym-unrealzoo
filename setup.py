1c00034ad1cba8d89dfd6cf38e3a38bb86d45abf
wukui-muc <wukui0099@gmail.com>
2024-12-30 13:25:53 +0800

    update file name

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_unrealcv',
       version='2.0.0',
-      install_requires=['gym<=0.21.0', 'matplotlib', 'numpy', 'unrealcv', 'wget', 'opencv-python','simple_pid', 'pynput'],  # And any other dependencies foo needs
+      install_requires=['gym==0.19.0', 'matplotlib', 'numpy', 'unrealcv', 'wget', 'opencv-python','simple_pid', 'pynput'],  # And any other dependencies foo needs
 )
